88cde4bd0f3901af1f46412dadb5fbf5a2d023f7
D3TONAT0R <gdv@swissonline.ch>
2023-05-14 22:37:48 +0200

    Improve block distribution analysis utility

--- ConsoleTools/BlockDistributionConsoleTool.cs
+++ ConsoleTools/BlockDistributionConsoleTool.cs
@@ -11,70 +11,10 @@ namespace MCUtils.ConsoleTools
 	public class BlockDistributionConsoleTool : IConsoleTool
 	{
 
-		static readonly string[] ores = new string[]
-		{
-			"Coal;coal_ore,deepslate_coal_ore",
-			"Iron;iron_ore,deepslate_iron_ore",
-			"Iron Block;raw_iron_block",
-			"Gold;gold_ore,deepslate_gold_ore",
-			"Gold Block;raw_gold_block",
-			"Copper;copper_ore,deepslate_copper_ore",
-			"Copper Block;raw_copper_block",
-			"Diamond;diamond_ore,deepslate_diamond_ore",
-			"Emerald;emerald_ore,deepslate_emerald_ore",
-			"Lapis Lazuli;lapis_ore,deepslate_lapis_ore",
-			"Redstone;redstone_ore,deepslate_redstone_ore",
-			"Nether Quartz;nether_quartz_ore",
-			"Nether Gold;nether_gold_ore",
-			"Ancient Debris;ancient_debris",
-			"Amethyst;amethyst_block,budding_amethyst"
-		};
-
-		static readonly string[] airAndLiquids = new string[]
-		{
-			"Air;air,cave_air",
-			"Water;water",
-			"Lava;lava"
-		};
-
-		static readonly string[] stones = new string[]
-		{
-			"Stone;stone",
-			"Deepslate;deepslate",
-			"Sandstone;sandstone",
-			"Andesite;andesite",
-			"Diorite;diorite",
-			"Granite;granite",
-			"Tuff;tuff",
-			"Calcite;calcite",
-			"Basalt;basalt,smooth_basalt"
-		};
-
-		static readonly string[] terrainBlocks = new string[]
-		{
-			"Dirt;dirt,grass_block,podzol,rooted_dirt,coarse_dirt",
-			"Gravel;gravel",
-			"Sand;sand,red_sand",
-			"Clay;clay",
-			"Mud;mud"
-		};
-
-		class BlockGroup
-		{
-			public string name;
-			public ProtoBlock[] blocks;
-
-			public BlockGroup(string name, ProtoBlock[] blocks)
-			{
-				this.name = name;
-				this.blocks = blocks;
-			}
-		}
-
 		public void Run(string[] args)
 		{
 			string path;
-			if (args.Length > 0)
+			if(args.Length > 0)
 			{
 				path = args[0];
 			}
@@ -90,142 +30,64 @@ namespace MCUtils.ConsoleTools
 			Console.WriteLine("[3] Ores + Air + Liquids + Stones");
 			Console.WriteLine("[4] Ores + Air + Liquids + Stones + Terrain");
 			int choice;
-			while (!GetChoice(out choice))
+			while(!GetChoice(out choice))
 			{
 				Console.WriteLine("Invalid input.");
 			}
 
-			List<BlockGroup> groups = new List<BlockGroup>();
-			foreach (var q in ores) RegisterGroup(q, groups);
-			if (choice >= 2) foreach (var q in airAndLiquids) RegisterGroup(q, groups);
-			if (choice >= 3) foreach (var q in stones) RegisterGroup(q, groups);
-			if (choice >= 4) foreach (var q in terrainBlocks) RegisterGroup(q, groups);
+			BlockDistributionAnalysis.TargetBlockTypes targetFlags = BlockDistributionAnalysis.TargetBlockTypes.Ores;
+			if(choice >= 2) targetFlags |= BlockDistributionAnalysis.TargetBlockTypes.AirAndLiquids;
+			if(choice >= 3) targetFlags |= BlockDistributionAnalysis.TargetBlockTypes.Stones;
+			if(choice >= 4) targetFlags |= BlockDistributionAnalysis.TargetBlockTypes.TerrainBlocks;
 
 			World.GetWorldInfo(path, out var worldName, out var version, out var regionLocations);
+			Console.WriteLine($"World: {worldName}, Version: {version}");
 
-			Console.WriteLine($"World: {worldName}, Version {version}");
-
-			CountSeparateYLevels(groups, path, regionLocations, -64, 320);
+			PerformAnalysis(targetFlags, path, regionLocations, -64, 320);
 		}
 
-		static void RegisterGroup(string queryInput, List<BlockGroup> groups)
+		static void PerformAnalysis(BlockDistributionAnalysis.TargetBlockTypes targetFlags, string worldRoot, RegionLocation[] regions, short yMin, short yMax)
 		{
-			var split = queryInput.Split(';');
-			var blocks = new List<ProtoBlock>(split[1].Split(',').Select(s => BlockList.Find(s)));
-			blocks.RemoveAll(b => b == null);
-			groups.Add(new BlockGroup(split[0], blocks.ToArray()));
-		}
-
-		static void CountSeparateYLevels(List<BlockGroup> groups, string worldRoot, RegionLocation[] regions, short yMin, short yMax)
-		{
-			var searchQuery = new List<ProtoBlock>();
-			foreach (var g in groups)
-			{
-				foreach (var b in g.blocks)
-				{
-					if (!searchQuery.Contains(b)) searchQuery.Add(b);
-				}
-			}
-			int countedChunks = 0;
-			Dictionary<ProtoBlock, Dictionary<short, long>> counts = new Dictionary<ProtoBlock, Dictionary<short, long>>();
-			foreach (var pb in searchQuery)
-			{
-				Dictionary<short, long> counter = new Dictionary<short, long>();
-				for (short y = yMin; y < yMax; y++)
-				{
-					counter.Add(y, 0);
-				}
-				counts.Add(pb, counter);
-			}
-
-			for (int ri = 0; ri < regions.Length; ri++)
+			BlockDistributionAnalysis analysis = new BlockDistributionAnalysis(targetFlags, yMin, yMax);
+			for(int ri = 0; ri < regions.Length; ri++)
 			{
 				Console.WriteLine($"Processing region {regions[ri].ToFileName()} [{ri + 1}/{regions.Length}]");
 				try
 				{
 					var region = RegionLoader.LoadRegion(Path.Combine(worldRoot, "region", regions[ri].ToFileName()));
 					Console.WriteLine("Region loaded, processing chunks ...");
-					Parallel.For(0, 1024, i => {
-						var c = region.chunks[i % 32, i / 32];
-						if (c != null)
-						{
-							try
-							{
-								foreach (var kv in c.sections)
-								{
-									var section = kv.Value;
-									short baseY = (short)(kv.Key * 16);
-									for (byte y = 0; y < 16; y++)
-									{
-										short worldY = (short)(baseY + y);
-										if (worldY < yMin || worldY >= yMax) continue;
-										for (byte z = 0; z < 16; z++)
-										{
-											for (byte x = 0; x < 16; x++)
-											{
-												var b = section.GetBlockAt(x, y, z).block;
-												if (b != null && counts.TryGetValue(b, out var stat))
-												{
-													stat[worldY]++;
-												}
-											}
-										}
-									}
-								}
-								countedChunks++;
-							}
-							catch (Exception e)
-							{
-								Console.WriteLine($"Chunk read failed @ r.{region.regionPos.x}.{region.regionPos.z} [{c.coords}]: {e.Message}");
-							}
-						}
-					});
+					analysis.AnalyzeRegion(region);
 				}
-				catch (Exception e)
+				catch(Exception e)
 				{
-					Console.WriteLine("Region processing failed, skipping ...");
+					Console.WriteLine($"Failed to analyze region at {regions[ri].x}.{regions[ri].z}: {e.Message}");
 				}
 			}
-			Console.WriteLine("Preparing data ...");
-			char sep = ',';
-			StringBuilder csv = new StringBuilder();
-			ProtoBlock[] queries = counts.Keys.ToArray();
-			csv.Append("");
-			foreach (var group in groups)
-			{
-				csv.Append(sep + group.name);
-			}
-			csv.Append(sep + "Chunk count: " + countedChunks);
-			csv.AppendLine();
-			for (short y = yMin; y < yMax; y++)
-			{
-				csv.Append(y);
-				foreach (var g in groups)
-				{
-					long counted = 0;
-					foreach (var b in g.blocks) counted += counts[b][y];
-					//value = amount of blocks per chunk
-					double amount = counted / (double)countedChunks;
-					csv.Append(sep + amount.ToString());
-				}
-				csv.AppendLine();
-			}
-			string filename = Path.Combine(worldRoot, "ore_distribution.csv");
-			File.WriteAllText(filename, csv.ToString());
-			Console.WriteLine("Data written to " + filename);
+			analysis.SaveAsCSVInWorldFolder(worldRoot);
+			Console.WriteLine("Saved to " + worldRoot);
 		}
 
 		static bool GetChoice(out int choice)
 		{
 			var k = Console.ReadKey(true).KeyChar;
-			if (k == '1')
+			if(k == '1')
 			{
 				choice = 1;
 				return true;
 			}
-			else if (k == '2')
+			else if(k == '2')
 			{
 				choice = 2;
+				return true;
+			}
+			else if(k == '3')
+			{
+				choice = 3;
+				return true;
+			}
+			else if(k == '4')
+			{
+				choice = 4;
 				return true;
 			}
 			else
---
+++ Utilities/BlockDistributionAnalysis.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCUtils
+{
+	public class BlockDistributionAnalysis
+	{
+		[Flags]
+		public enum TargetBlockTypes
+		{
+			None = 0,
+			Ores = 1,
+			AirAndLiquids = 2,
+			Stones = 3,
+			TerrainBlocks = 4
+		}
+
+		static readonly string[] ores = new string[]
+		{
+			"Coal;coal_ore,deepslate_coal_ore",
+			"Iron;iron_ore,deepslate_iron_ore",
+			"Iron Block;raw_iron_block",
+			"Gold;gold_ore,deepslate_gold_ore",
+			"Gold Block;raw_gold_block",
+			"Copper;copper_ore,deepslate_copper_ore",
+			"Copper Block;raw_copper_block",
+			"Diamond;diamond_ore,deepslate_diamond_ore",
+			"Emerald;emerald_ore,deepslate_emerald_ore",
+			"Lapis Lazuli;lapis_ore,deepslate_lapis_ore",
+			"Redstone;redstone_ore,deepslate_redstone_ore",
+			"Nether Quartz;nether_quartz_ore",
+			"Nether Gold;nether_gold_ore",
+			"Ancient Debris;ancient_debris",
+			"Amethyst;amethyst_block,budding_amethyst"
+		};
+
+		static readonly string[] airAndLiquids = new string[]
+		{
+			"Air;air,cave_air",
+			"Water;water",
+			"Lava;lava"
+		};
+
+		static readonly string[] stones = new string[]
+		{
+			"Stone;stone",
+			"Deepslate;deepslate",
+			"Sandstone;sandstone",
+			"Andesite;andesite",
+			"Diorite;diorite",
+			"Granite;granite",
+			"Tuff;tuff",
+			"Calcite;calcite",
+			"Basalt;basalt,smooth_basalt"
+		};
+
+		static readonly string[] terrainBlocks = new string[]
+		{
+			"Dirt;dirt,grass_block,podzol,rooted_dirt,coarse_dirt",
+			"Gravel;gravel",
+			"Sand;sand,red_sand",
+			"Clay;clay",
+			"Mud;mud"
+		};
+
+		public class BlockGroup
+		{
+			public string name;
+			public ProtoBlock[] blocks;
+
+			public BlockGroup(string name, ProtoBlock[] blocks)
+			{
+				this.name = name;
+				this.blocks = blocks;
+			}
+		}
+
+		public TargetBlockTypes BlockTypes { get; private set; }
+		public short YMin { get; private set; }
+		public short YMax { get; private set; }
+		public int CountedChunks { get; private set; }
+
+		private List<BlockGroup> blockGroups;
+		public Dictionary<ProtoBlock, Dictionary<short, long>> results;
+
+		public static List<BlockGroup> GetTargetBlocks(TargetBlockTypes typeFlags)
+		{
+			List<BlockGroup> groups = new List<BlockGroup>();
+			if(typeFlags.HasFlag(TargetBlockTypes.Ores)) RegisterGroups(ores, groups);
+			if(typeFlags.HasFlag(TargetBlockTypes.AirAndLiquids)) RegisterGroups(airAndLiquids, groups);
+			if(typeFlags.HasFlag(TargetBlockTypes.Stones)) RegisterGroups(stones, groups);
+			if(typeFlags.HasFlag(TargetBlockTypes.TerrainBlocks)) RegisterGroups(terrainBlocks, groups);
+			return groups;
+		}
+
+		private static void RegisterGroups(string[] input, List<BlockGroup> groups)
+		{
+			foreach(var g in input) RegisterGroup(g, groups);
+		}
+
+		private static void RegisterGroup(string queryInput, List<BlockGroup> groups)
+		{
+			var split = queryInput.Split(';');
+			var blocks = new List<ProtoBlock>(split[1].Split(',').Select(s => BlockList.Find(s)));
+			blocks.RemoveAll(b => b == null);
+			groups.Add(new BlockGroup(split[0], blocks.ToArray()));
+		}
+
+		public BlockDistributionAnalysis(List<BlockGroup> groups, short yMin = -64, short yMax = 320)
+		{
+			blockGroups = groups;
+			var searchQuery = new List<ProtoBlock>();
+			foreach(var g in groups)
+			{
+				foreach(var b in g.blocks)
+				{
+					if(!searchQuery.Contains(b)) searchQuery.Add(b);
+				}
+			}
+
+			YMin = yMin;
+			YMax = yMax;
+			results = new Dictionary<ProtoBlock, Dictionary<short, long>>();
+			foreach(var pb in searchQuery)
+			{
+				Dictionary<short, long> counter = new Dictionary<short, long>();
+				for(short y = YMin; y < YMax; y++)
+				{
+					counter.Add(y, 0);
+				}
+				results.Add(pb, counter);
+			}
+		}
+
+		public BlockDistributionAnalysis(TargetBlockTypes targetFlags, short yMin = -64, short yMax = 320) : this(GetTargetBlocks(targetFlags), yMin, yMax)
+		{
+
+		}
+
+		public void AnalyzeChunk(ChunkData chunk)
+		{
+			try
+			{
+				/*
+				foreach(var kv in chunk.sections)
+				{
+					var section = kv.Value;
+					short baseY = (short)(kv.Key * 16);
+					for(byte y = 0; y < 16; y++)
+					{
+						short worldY = (short)(baseY + y);
+						if(worldY < YMin || worldY >= YMax) continue;
+						for(byte z = 0; z < 16; z++)
+						{
+							for(byte x = 0; x < 16; x++)
+							{
+								var b = section.GetBlockAt(x, y, z).block;
+								if(b != null && results.TryGetValue(b, out var stat))
+								{
+									stat[worldY]++;
+								}
+							}
+						}
+					}
+				}
+				*/
+				if(chunk.HasTerrain)
+				{
+					for(byte z = 0; z < 16; z++)
+					{
+						for(byte x = 0; x < 16; x++)
+						{
+							for(short y = YMin; y < YMax; y++)
+							{
+								var b = chunk.GetBlockAt(x, y, z);
+								if(b != null && results.TryGetValue(b.block, out var stat))
+								{
+									stat[y]++;
+								}
+							}
+						}
+					}
+					CountedChunks++;
+				}
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine($"Failed to read chunk at {chunk.coords}: " + e.Message);
+			}
+		}
+
+		public void AnalyzeRegion(Region region)
+		{
+			Parallel.For(0, 1024, (i) =>
+			{
+				var chunk = region.chunks[i % 32, i / 32];
+				if(chunk != null)
+				{
+					AnalyzeChunk(chunk);
+				}
+			});
+		}
+
+		public string GenerateResultsAsCSV()
+		{
+			char sep = ',';
+			StringBuilder csv = new StringBuilder();
+			ProtoBlock[] queries = results.Keys.ToArray();
+			csv.Append("");
+			foreach(var group in blockGroups)
+			{
+				csv.Append(sep + group.name);
+			}
+			csv.Append(sep + "Chunk count: " + CountedChunks);
+			csv.AppendLine();
+			for(short y = YMin; y < YMax; y++)
+			{
+				csv.Append(y);
+				foreach(var g in blockGroups)
+				{
+					long counted = 0;
+					foreach(var b in g.blocks) counted += results[b][y];
+					//value = amount of blocks per chunk
+					double amount = counted / (double)CountedChunks;
+					csv.Append(sep + amount.ToString());
+				}
+				csv.AppendLine();
+			}
+			return csv.ToString();
+		}
+
+		public void SaveAsCSVInWorldFolder(string worldFolder)
+		{
+			string csv = GenerateResultsAsCSV();
+			string path = Path.Combine(worldFolder, "block_distribution.csv");
+			File.WriteAllText(path, csv);
+			Console.WriteLine("Results written to " + path);
+		}
+	}
+}
