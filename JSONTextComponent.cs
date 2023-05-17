8b4eaf5da818ffd84e191a8d9afad2e39555ba45
D3TONAT0R <gdv@swissonline.ch>
2023-05-17 21:15:09 +0200

    Rename JSONText to JSONTextComponent

---
+++ JSONTextComponent.cs
@@ -0,0 +1,149 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCUtils
+{
+	public class JSONTextComponent
+	{
+		public static Dictionary<string, string> legacyFormattingColorCodes = new Dictionary<string, string>()
+		{
+			{"black", "§0" },
+			{"dark_blue", "§1" },
+			{"dark_green", "§2" },
+			{"dark_aqua", "§3" },
+			{"dark_red", "§4" },
+			{"dark_purple", "§5" },
+			{"gold", "§6" },
+			{"gray", "§7" },
+			{"dark_gray", "§8" },
+			{"blue", "§9" },
+			{"green", "§a" },
+			{"aqua", "§b" },
+			{"red", "§c" },
+			{"light_purple", "§d" },
+			{"yellow", "§e" },
+			{"white", "§f" }
+		};
+		public const string legacyObfuscationFormatKey = "§k";
+		public const string legacyBoldFormatKey = "§l";
+		public const string legacyStrikethroughKey = "§m";
+		public const string legacyUnderlinehKey = "§n";
+		public const string legacyItalichKey = "§o";
+		public const string legacyResetKey = "§r";
+
+		public List<Dictionary<string, object>> data;
+
+		private JSONTextComponent()
+		{
+
+		}
+
+		public JSONTextComponent(string text)
+		{
+			data = new List<Dictionary<string, object>>
+			{
+				new Dictionary<string, object>() { { "text", text } }
+			};
+		}
+
+		public JSONTextComponent(string text, string color)
+		{
+			data = new List<Dictionary<string, object>>
+			{
+				new Dictionary<string, object>() {
+					{ "text", text },
+					{ "color", color }
+				}
+			};
+		}
+
+		public static JSONTextComponent Parse(string json)
+		{
+			return new JSONTextComponent
+			{
+				data = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(json)
+			};
+		}
+
+		public string ToJSON()
+		{
+			if(data.Count == 1 && data[0].Count == 1 && data[0].ContainsKey("text"))
+			{
+				return $"\"{data[0]["text"]}\"";
+			}
+			else
+			{
+				return JArray.FromObject(data).ToString();
+			}
+		}
+
+		public string ToLegacyPlainText()
+		{
+			//TODO: recursively check for "text" components
+			StringBuilder text = new StringBuilder();
+			string lastColorKey = null;
+			bool bold = false;
+			bool italic = false;
+			bool underlined = false;
+			bool strikethrough = false;
+			bool obfuscated = false;
+			List<string> formatKeyQueue = new List<string>();
+			foreach(var d in data)
+			{
+
+				bool reset = false;
+				reset |= ApplyFormatting(formatKeyQueue, d, "bold", legacyBoldFormatKey, ref bold);
+				reset |= ApplyFormatting(formatKeyQueue, d, "italic", legacyItalichKey, ref italic);
+				reset |= ApplyFormatting(formatKeyQueue, d, "underlined", legacyUnderlinehKey, ref underlined);
+				reset |= ApplyFormatting(formatKeyQueue, d, "strikethrough", legacyStrikethroughKey, ref strikethrough);
+				reset |= ApplyFormatting(formatKeyQueue, d, "obfuscated", legacyObfuscationFormatKey, ref obfuscated);
+
+				if(reset)
+				{
+					text.Append(legacyResetKey);
+				}
+
+				string newColorKey = null;
+				if(d.TryGetValue("color", out var obj))
+				{
+					var color = obj as string;
+					if(!color.StartsWith("#") && legacyFormattingColorCodes.TryGetValue(color, out string key))
+					{
+						newColorKey = key;
+					}
+				}
+				else
+				{
+					newColorKey = lastColorKey;
+				}
+
+				if(newColorKey != null)
+				{
+					text.Append(newColorKey);
+				}
+				lastColorKey = newColorKey;
+
+				if(d.TryGetValue("text", out object s))
+				{
+					text.Append(s.ToString());
+				}
+			}
+			return text.ToString();
+		}
+
+		private bool ApplyFormatting(List<string> queue, Dictionary<string, object> d, string key, string legacyFormatKey, ref bool currentState)
+		{
+			bool reset = false;
+			if(d.TryGetValue(key, out var obj))
+			{
+				bool b = (string)obj == "true";
+				if(b) queue.Add(legacyFormatKey);
+				else if(currentState) reset = true;
+				currentState = b;
+			}
+			return reset;
+		}
+	}
+}
--- TileEntities/TileEntitySign.cs
+++ TileEntities/TileEntitySign.cs
@@ -1,163 +1,17 @@
 ﻿using MCUtils.Coordinates;
 using MCUtils.NBT;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MCUtils.TileEntities
 {
-	public class TileEntitySign : TileEntity
+	public partial class TileEntitySign : TileEntity
 	{
-		public class JSONText
-		{
-			public static Dictionary<string, string> legacyFormattingColorCodes = new Dictionary<string, string>()
-			{
-				{"black", "§0" },
-				{"dark_blue", "§1" },
-				{"dark_green", "§2" },
-				{"dark_aqua", "§3" },
-				{"dark_red", "§4" },
-				{"dark_purple", "§5" },
-				{"gold", "§6" },
-				{"gray", "§7" },
-				{"dark_gray", "§8" },
-				{"blue", "§9" },
-				{"green", "§a" },
-				{"aqua", "§b" },
-				{"red", "§c" },
-				{"light_purple", "§d" },
-				{"yellow", "§e" },
-				{"white", "§f" }
-			};
-			public const string legacyObfuscationFormatKey = "§k";
-			public const string legacyBoldFormatKey = "§l";
-			public const string legacyStrikethroughKey = "§m";
-			public const string legacyUnderlinehKey = "§n";
-			public const string legacyItalichKey = "§o";
-			public const string legacyResetKey = "§r";
-
-			public List<Dictionary<string, object>> data;
-
-			private JSONText()
-			{
-
-			}
-
-			public JSONText(string text)
-			{
-				data = new List<Dictionary<string, object>>
-				{
-					new Dictionary<string, object>() { { "text", text } }
-				};
-			}
-
-			public JSONText(string text, string color)
-			{
-				data = new List<Dictionary<string, object>>
-				{
-					new Dictionary<string, object>() {
-						{ "text", text },
-						{ "color", color }
-					}
-				};
-			}
-
-			public static JSONText Parse(string json)
-			{
-				return new JSONText
-				{
-					data = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(json)
-				};
-			}
-
-			public string ToJSON()
-			{
-				if(data.Count == 1 && data[0].Count == 1 && data[0].ContainsKey("text"))
-				{
-					return $"\"{data[0]["text"]}\"";
-				}
-				else
-				{
-					return JArray.FromObject(data).ToString();
-				}
-			}
-
-			public string ToLegacyPlainText()
-			{
-				//TODO: recursively check for "text" components
-				StringBuilder text = new StringBuilder();
-				string lastColorKey = null;
-				bool bold = false;
-				bool italic = false;
-				bool underlined = false;
-				bool strikethrough = false;
-				bool obfuscated = false;
-				List<string> formatKeyQueue = new List<string>();
-				foreach(var d in data)
-				{
-
-					bool reset = false;
-					reset |= ApplyFormatting(formatKeyQueue, d, "bold", legacyBoldFormatKey, ref bold);
-					reset |= ApplyFormatting(formatKeyQueue, d, "italic", legacyItalichKey, ref italic);
-					reset |= ApplyFormatting(formatKeyQueue, d, "underlined", legacyUnderlinehKey, ref underlined);
-					reset |= ApplyFormatting(formatKeyQueue, d, "strikethrough", legacyStrikethroughKey, ref strikethrough);
-					reset |= ApplyFormatting(formatKeyQueue, d, "obfuscated", legacyObfuscationFormatKey, ref obfuscated);
-
-					if(reset)
-					{
-						text.Append(legacyResetKey);
-					}
-
-					string newColorKey = null;
-					if(d.TryGetValue("color", out var obj))
-					{
-						var color = obj as string;
-						if(!color.StartsWith("#") && legacyFormattingColorCodes.TryGetValue(color, out string key))
-						{
-							newColorKey = key;
-						}
-					}
-					else
-					{
-						newColorKey = lastColorKey;
-					}
-
-					if(newColorKey != null)
-					{
-						text.Append(newColorKey);
-					}
-					lastColorKey = newColorKey;
-
-					if(d.TryGetValue("text", out object s))
-					{
-						text.Append(s.ToString());
-					}
-				}
-				return text.ToString();
-			}
-
-			private bool ApplyFormatting(List<string> queue, Dictionary<string, object> d, string key, string legacyFormatKey, ref bool currentState)
-			{
-				bool reset = false;
-				if(d.TryGetValue(key, out var obj))
-				{
-					bool b = (string)obj == "true";
-					if(b) queue.Add(legacyFormatKey);
-					else if(currentState) reset = true;
-					currentState = b;
-				}
-				return reset;
-			}
-		}
-
 		public struct SignData
 		{
-			public JSONText line1;
-			public JSONText line2;
-			public JSONText line3;
-			public JSONText line4;
+			public JSONTextComponent line1;
+			public JSONTextComponent line2;
+			public JSONTextComponent line3;
+			public JSONTextComponent line4;
 
 			//Added in 1.14, defaults to black
 			public string color;
@@ -166,10 +20,10 @@ namespace MCUtils.TileEntities
 
 			public SignData(string line1Text = "", string line2Text = "", string line3Text = "", string line4Text = "")
 			{
-				line1 = new JSONText(line1Text);
-				line2 = new JSONText(line2Text);
-				line3 = new JSONText(line3Text);
-				line4 = new JSONText(line4Text);
+				line1 = new JSONTextComponent(line1Text);
+				line2 = new JSONTextComponent(line2Text);
+				line3 = new JSONTextComponent(line3Text);
+				line4 = new JSONTextComponent(line4Text);
 				color = "black";
 				glowingText = false;
 			}
@@ -204,11 +58,11 @@ namespace MCUtils.TileEntities
 				}
 			}
 
-			private string GetSignLineData(JSONText text, Version version)
+			private string GetSignLineData(JSONTextComponent text, Version version)
 			{
 				if(version < Version.Release_1(8))
 				{
-					return text?.ToPlainText() ?? "";
+					return text?.ToLegacyPlainText() ?? "";
 				}
 				else
 				{
@@ -223,10 +77,10 @@ namespace MCUtils.TileEntities
 					//1.20+ format
 					if(nbt.TryGet("messages", out string[] linesJson))
 					{
-						if(linesJson.Length >= 1) line1 = JSONText.Parse(linesJson[0]);
-						if(linesJson.Length >= 2) line1 = JSONText.Parse(linesJson[1]);
-						if(linesJson.Length >= 3) line1 = JSONText.Parse(linesJson[2]);
-						if(linesJson.Length >= 4) line1 = JSONText.Parse(linesJson[3]);
+						if(linesJson.Length >= 1) line1 = JSONTextComponent.Parse(linesJson[0]);
+						if(linesJson.Length >= 2) line1 = JSONTextComponent.Parse(linesJson[1]);
+						if(linesJson.Length >= 3) line1 = JSONTextComponent.Parse(linesJson[2]);
+						if(linesJson.Length >= 4) line1 = JSONTextComponent.Parse(linesJson[3]);
 					}
 					nbt.TryGet("color", out color);
 					nbt.TryGet("has_glowing_text", out glowingText);
@@ -237,17 +91,17 @@ namespace MCUtils.TileEntities
 					//TODO: how to determine pre-1.8 versions? 'version' becomes null prior to 1.9 (due to DataVersion).
 					if(version.HasValue && version >= Version.Release_1(8))
 					{
-						line1 = JSONText.Parse("Text1");
-						line2 = JSONText.Parse("Text2");
-						line3 = JSONText.Parse("Text3");
-						line4 = JSONText.Parse("Text4");
+						line1 = JSONTextComponent.Parse("Text1");
+						line2 = JSONTextComponent.Parse("Text2");
+						line3 = JSONTextComponent.Parse("Text3");
+						line4 = JSONTextComponent.Parse("Text4");
 					}
 					else
 					{
-						line1 = new JSONText(nbt.Get<string>("Text1"));
-						line2 = new JSONText(nbt.Get<string>("Text2"));
-						line3 = new JSONText(nbt.Get<string>("Text3"));
-						line4 = new JSONText(nbt.Get<string>("Text4"));
+						line1 = new JSONTextComponent(nbt.Get<string>("Text1"));
+						line2 = new JSONTextComponent(nbt.Get<string>("Text2"));
+						line3 = new JSONTextComponent(nbt.Get<string>("Text3"));
+						line4 = new JSONTextComponent(nbt.Get<string>("Text4"));
 					}
 					nbt.TryGet("Color", out color);
 					nbt.TryGet("GlowingText", out glowingText);
@@ -274,7 +128,6 @@ namespace MCUtils.TileEntities
 
 		public TileEntitySign(NBTCompound nbt, Version? version, out BlockCoord blockPos) : base(nbt, out blockPos)
 		{
-			bool isNewFormat = nbt.Contains("is_waxed") || nbt.Contains("front_text");
 			if(version.HasValue && version.Value >= Version.Release_1(20))
 			{
 				nbt.TryTake("is_waxed", out isWaxed);
