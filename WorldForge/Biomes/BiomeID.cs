6d5e83de6aa4d52ba024923296cb2d0611d1739f
D3TONAT0R <gdv@swissonline.ch>
2024-10-04 17:10:52 +0200

    Rewrite biomes

--- WorldForge/Biomes/BiomeID.cs
+++ WorldForge/Biomes/BiomeID.cs
@@ -1,110 +1,128 @@
 ﻿namespace WorldForge.Biomes
 {
-	public enum BiomeID : byte
+	public class BiomeID
 	{
-		ocean = 0,
-		plains = 1,
-		desert = 2,
-		extreme_hills = 3,
-		mountains = 3,
-		windswept_hills = 3,
-		forest = 4,
-		taiga = 5,
-		swamp = 6,
-		river = 7,
-		nether_wastes = 8,
-		the_end = 9,
-		frozen_ocean = 10,
-		frozen_river = 11,
-		snowy_tundra = 12,
-		snowy_mountains = 13,
-		mushroom_fields = 14,
-		mushroom_field_shore = 15,
-		beach = 16,
-		desert_hills = 17,
-		wooded_hills = 18,
-		taiga_hills = 19,
-		mountain_edge = 20,
-		jungle = 21,
-		jungle_hills = 22,
-		jungle_edge = 23,
-		deep_ocean = 24,
-		stone_shore = 25,
-		snowy_beach = 26,
-		birch_forest = 27,
-		birch_forest_hills = 28,
-		dark_forest = 29,
-		snowy_taiga = 30,
-		snowy_taiga_hills = 31,
-		giant_tree_taiga = 32,
-		giant_tree_taiga_hills = 33,
-		wooded_mountains = 34,
-		savanna = 35,
-		savanna_plateau = 36,
-		badlands = 37,
-		wooded_badlands_plateau = 38,
-		badlands_plateau = 39,
-		small_end_islands = 40,
-		end_midlands = 41,
-		end_highlands = 42,
-		end_barrens = 43,
-		warm_ocean = 44,
-		lukewarm_ocean = 45,
-		cold_ocean = 46,
-		deep_warm_ocean = 47,
-		deep_lukewarm_ocean = 48,
-		deep_cold_ocean = 49,
-		deep_frozen_ocean = 50,
-		the_void = 127,
-		sunflower_plains = 129,
-		desert_lakes = 130,
-		gravelly_mountains = 131,
-		flower_forest = 132,
-		taiga_mountains = 133,
-		swamp_hills = 134,
-		ice_spikes = 140,
-		modified_jungle = 149,
-		modified_jungle_edge = 151,
-		tall_birch_forest = 155,
-		tall_birch_hills = 156,
-		dark_forest_hills = 157,
-		snowy_taiga_mountains = 158,
-		giant_spruce_taiga = 160,
-		giant_spruce_taiga_hills = 161,
-		modified_gravelly_mountains = 162,
-		shattered_savanna = 163,
-		shattered_savanna_plateau = 164,
-		eroded_badlands = 165,
-		modified_wooded_badlands_plateau = 166,
-		modified_badlands_plateau = 167,
-		bamboo_jungle = 168,
-		bamboo_jungle_hills = 169,
-		soul_sand_valley = 170,
-		crimson_forest = 171,
-		warped_forest = 172,
-		basalt_deltas = 173,
-		dripstone_caves = 174,
-		lush_caves = 175,
-		stony_shore,
-		deep_dark,
-		meadow,
-		old_growth_birch_forest,
+		//TODO: load biomes from resource file
+		public static readonly BiomeID ocean = new BiomeID("ocean", 0);
+		public static readonly BiomeID plains = new BiomeID("plains", 1);
+		public static readonly BiomeID desert = new BiomeID("desert", 2);
+		public static readonly BiomeID extremeHills = new BiomeID("extreme_hills", 3);
+		public static readonly BiomeID mountains = new BiomeID("mountains", 3);
+		public static readonly BiomeID windsweptHills = new BiomeID("windswept_hills", 3);
+		public static readonly BiomeID forest = new BiomeID("forest", 4);
+		public static readonly BiomeID taiga = new BiomeID("taiga", 5);
+		public static readonly BiomeID swamp = new BiomeID("swamp", 6);
+		public static readonly BiomeID river = new BiomeID("river", 7);
+		public static readonly BiomeID netherWastes = new BiomeID("nether_wastes", 8);
+		public static readonly BiomeID theEnd = new BiomeID("the_end", 9);
+		public static readonly BiomeID frozenOcean = new BiomeID("frozen_ocean", 10);
+		public static readonly BiomeID frozenRiver = new BiomeID("frozen_river", 11);
+		public static readonly BiomeID snowyTundra = new BiomeID("snowy_tundra", 12);
+		public static readonly BiomeID snowyMountains = new BiomeID("snowy_mountains", 13);
+		public static readonly BiomeID mushroomFields = new BiomeID("mushroom_fields", 14);
+		public static readonly BiomeID mushroomFieldShore = new BiomeID("mushroom_field_shore", 15);
+		public static readonly BiomeID beach = new BiomeID("beach", 16);
+		public static readonly BiomeID desertHills = new BiomeID("desert_hills", 17);
+		public static readonly BiomeID woodedHills = new BiomeID("wooded_hills", 18);
+		public static readonly BiomeID taigaHills = new BiomeID("taiga_hills", 19);
+		public static readonly BiomeID mountainEdge = new BiomeID("mountain_edge", 20);
+		public static readonly BiomeID jungle = new BiomeID("jungle", 21);
+		public static readonly BiomeID jungleHills = new BiomeID("jungle_hills", 22);
+		public static readonly BiomeID jungleEdge = new BiomeID("jungle_edge", 23);
+		public static readonly BiomeID deepOcean = new BiomeID("deep_ocean", 24);
+		public static readonly BiomeID stoneShore = new BiomeID("stone_shore", 25);
+		public static readonly BiomeID snowyBeach = new BiomeID("snowy_beach", 26);
+		public static readonly BiomeID birchForest = new BiomeID("birch_forest", 27);
+		public static readonly BiomeID birchForestHills = new BiomeID("birch_forest_hills", 28);
+		public static readonly BiomeID darkForest = new BiomeID("dark_forest", 29);
+		public static readonly BiomeID snowyTaiga = new BiomeID("snowy_taiga", 30);
+		public static readonly BiomeID snowyTaigaHills = new BiomeID("snowy_taiga_hills", 31);
+		public static readonly BiomeID giantTreeTaiga = new BiomeID("giant_tree_taiga", 32);
+		public static readonly BiomeID giantTreeTaigaHills = new BiomeID("giant_tree_taiga_hills", 33);
+		public static readonly BiomeID woodedMountains = new BiomeID("wooded_mountains", 34);
+		public static readonly BiomeID savanna = new BiomeID("savanna", 35);
+		public static readonly BiomeID savannaPlateau = new BiomeID("savanna_plateau", 36);
+		public static readonly BiomeID badlands = new BiomeID("badlands", 37);
+		public static readonly BiomeID woodedBadlandsPlateau = new BiomeID("wooded_badlands_plateau", 38);
+		public static readonly BiomeID badlandsPlateau = new BiomeID("badlands_plateau", 39);
+		public static readonly BiomeID smallEndIslands = new BiomeID("small_end_islands", 40);
+		public static readonly BiomeID endMidlands = new BiomeID("end_midlands", 41);
+		public static readonly BiomeID endHighlands = new BiomeID("end_highlands", 42);
+		public static readonly BiomeID endBarrens = new BiomeID("end_barrens", 43);
+		public static readonly BiomeID warmOcean = new BiomeID("warm_ocean", 44);
+		public static readonly BiomeID lukewarmOcean = new BiomeID("lukewarm_ocean", 45);
+		public static readonly BiomeID coldOcean = new BiomeID("cold_ocean", 46);
+		public static readonly BiomeID deepWarmOcean = new BiomeID("deep_warm_ocean", 47);
+		public static readonly BiomeID deepLukewarmOcean = new BiomeID("deep_lukewarm_ocean", 48);
+		public static readonly BiomeID deepColdOcean = new BiomeID("deep_cold_ocean", 49);
+		public static readonly BiomeID deepFrozenOcean = new BiomeID("deep_frozen_ocean", 50);
+		public static readonly BiomeID theVoid = new BiomeID("the_void", 127);
+		public static readonly BiomeID sunflowerPlains = new BiomeID("sunflower_plains", 129);
+		public static readonly BiomeID desertLakes = new BiomeID("desert_lakes", 130);
+		public static readonly BiomeID gravellyMountains = new BiomeID("gravelly_mountains", 131);
+		public static readonly BiomeID flowerForest = new BiomeID("flower_forest", 132);
+		public static readonly BiomeID taigaMountains = new BiomeID("taiga_mountains", 133);
+		public static readonly BiomeID swampHills = new BiomeID("swamp_hills", 134);
+		public static readonly BiomeID iceSpikes = new BiomeID("ice_spikes", 140);
+		public static readonly BiomeID modifiedJungle = new BiomeID("modified_jungle", 149);
+		public static readonly BiomeID modifiedJungleEdge = new BiomeID("modified_jungle_edge", 151);
+		public static readonly BiomeID tallBirchForest = new BiomeID("tall_birch_forest", 155);
+		public static readonly BiomeID tallBirchHills = new BiomeID("tall_birch_hills", 156);
+		public static readonly BiomeID darkForestHills = new BiomeID("dark_forest_hills", 157);
+		public static readonly BiomeID snowyTaigaMountains = new BiomeID("snowy_taiga_mountains", 158);
+		public static readonly BiomeID giantSpruceTaiga = new BiomeID("giant_spruce_taiga", 160);
+		public static readonly BiomeID giantSpruceTaigaHills = new BiomeID("giant_spruce_taiga_hills", 161);
+		public static readonly BiomeID modifiedGravellyMountains = new BiomeID("modified_gravelly_mountains", 162);
+		public static readonly BiomeID shatteredSavanna = new BiomeID("shattered_savanna", 163);
+		public static readonly BiomeID shatteredSavannaPlateau = new BiomeID("shattered_savanna_plateau", 164);
+		public static readonly BiomeID erodedBadlands = new BiomeID("eroded_badlands", 165);
+		public static readonly BiomeID modifiedWoodedBadlandsPlateau = new BiomeID("modified_wooded_badlands_plateau", 166);
+		public static readonly BiomeID modifiedBadlandsPlateau = new BiomeID("modified_badlands_plateau", 167);
+		public static readonly BiomeID bambooJungle = new BiomeID("bamboo_jungle", 168);
+		public static readonly BiomeID bambooJunlgeHills = new BiomeID("bamboo_jungle_hills", 169);
+		public static readonly BiomeID soulSandValley = new BiomeID("soul_sand_valley", 170);
+		public static readonly BiomeID crimsonForest = new BiomeID("crimson_forest", 171);
+		public static readonly BiomeID warpedForest = new BiomeID("warped_forest", 172);
+		public static readonly BiomeID basaltDeltas = new BiomeID("basalt_deltas", 173);
+		public static readonly BiomeID dripstoneCaves = new BiomeID("dripstone_caves", 174);
+		public static readonly BiomeID lushCaves = new BiomeID("lush_caves", 175);
+		public static readonly BiomeID stonyShore = new BiomeID("stony_shore");
+		public static readonly BiomeID deepDark = new BiomeID("deep_dark");
+		public static readonly BiomeID meadow = new BiomeID("meadow");
+		public static readonly BiomeID oldGrowthBirchForest = new BiomeID("old_growth_birch_forest");
+		public static readonly BiomeID windsweptGravellyHills = new BiomeID("windswept_gravelly_hills");
+		public static readonly BiomeID sparseJungle = new BiomeID("sparse_jungle");
+		public static readonly BiomeID windsweptForest = new BiomeID("windswept_forest");
+		public static readonly BiomeID snowyPlains = new BiomeID("snowy_plains");
+		public static readonly BiomeID snowySlopes = new BiomeID("snowy_slopes");
+		public static readonly BiomeID stonyPeaks = new BiomeID("stony_peaks");
+		public static readonly BiomeID oldGrowthPineTaiga = new BiomeID("old_growth_pine_taiga");
+		public static readonly BiomeID frozenPeaks = new BiomeID("frozen_peaks");
+		public static readonly BiomeID grove = new BiomeID("grove");
+		public static readonly BiomeID jaggedPeaks = new BiomeID("jagged_peaks");
+		public static readonly BiomeID windsweptSavanna = new BiomeID("windswept_savanna");
+		public static readonly BiomeID mangroveSwamp = new BiomeID("mangrove_swamp");
+		public static readonly BiomeID cherryGrove = new BiomeID("cherry_grove");
+		public static readonly BiomeID oldGrowthSpruceTaiga = new BiomeID("old_growth_spruce_taiga");
+		public static readonly BiomeID woodedBadlands = new BiomeID("wooded_badlands");
 
-		//TODO: map IDs
-		windswept_gravelly_hills,
-		sparse_jungle,
-		windswept_forest,
-		snowy_plains,
-		snowy_slopes,
-		stony_peaks,
-		old_growth_pine_taiga,
-		frozen_peaks,
-		grove,
-		jagged_peaks,
-		windswept_savanna,
-		mangrove_swamp,
-		cherry_grove,
-		old_growth_spruce_taiga,
-		wooded_badlands
+		public readonly string id;
+		public readonly byte? numericId;
+
+		public BiomeID(string id, byte? numericId = null)
+		{
+			this.id = id;
+			this.numericId = numericId;
+		}
+
+		public override string ToString()
+		{
+			return id;
+		}
+
+		public override int GetHashCode()
+		{
+			return id.GetHashCode();
+		}
 	}
 }
