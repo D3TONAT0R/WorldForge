165d161cde827a8ed94c879ee6e69bb581856038
D3TONAT0R <gdv@swissonline.ch>
2026-01-17 20:13:17 +0100

    Render chunks from center

--- WorldForgeToolbox/RegionViewer.cs
+++ WorldForgeToolbox/RegionViewer.cs
@@ -1,29 +1,42 @@
+using System.Collections.Concurrent;
+using System.Drawing.Drawing2D;
 using WorldForge;
 using WorldForge.Coordinates;
 using WorldForge.IO;
+using Timer = System.Windows.Forms.Timer;
 
 namespace WorldForgeToolbox;
 
 public partial class RegionViewer : Form
 {
-    private Bitmap[,] chunkMaps = new Bitmap[32, 32];
-    private Random random = new Random();
+    private Bitmap?[] chunkMaps = new Bitmap?[1024];
     private string fileName;
     private WorldForge.Regions.Region region;
     private RegionLocation currentLocation;
 
     private ChunkCoord hoveredChunk = new ChunkCoord(-1, -1);
 
+    private List<int> chunkRenderQueue = new List<int>();
+    private List<int> currentChunkRenders = new List<int>();
+    private object currentChunkRendersLock = new object();
+
+    private Timer updateRenderTimer;
+
+    private Brush renderingChunkBrush = new SolidBrush(Color.FromArgb(160, 128, 128, 128));
+    private Brush queuedChunkBrush = new HatchBrush(HatchStyle.Percent50, Color.FromArgb(64, 128, 128, 128), Color.FromArgb(128, 128, 128, 128));
+
     public RegionViewer(string file)
     {
         InitializeComponent();
+        updateRenderTimer = new Timer() { Interval = 100 };
+        updateRenderTimer.Tick += (sender, eventArgs) => HandleRenders();
         fileName = file;
-        if(string.IsNullOrEmpty(fileName))
+        if (string.IsNullOrEmpty(fileName))
         {
             //Open file dialog to select region file
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Region Files (*.mca;*.mcr)|*.mca;*.mcr|All Files (*.*)|*.*";
-            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 fileName = openFileDialog.FileName;
             }
@@ -38,67 +51,110 @@ public partial class RegionViewer : Form
 
     private void Load()
     {
+        currentChunkRenders.Clear();
+        chunkRenderQueue.Clear();
+        //Queue renders, starting from center
+        chunkRenderQueue.AddRange(Enumerable.Range(0, 1024).OrderBy(i =>
+        {
+            int x = i % 32;
+            int z = i / 32;
+            x -= 16;
+            z -= 16;
+            return Math.Max(Math.Abs(x), Math.Abs(z));
+        }));
         RegionLocation.TryGetFromFileName(Path.GetFileName(fileName), out currentLocation);
         //Clear chunk maps
-        for(int x = 0; x < 32; x++)
+        for (int i = 0; i < chunkMaps.Length; i++)
         {
-            for(int z = 0; z < 32; z++)
-            {
-                chunkMaps[x, z] = null;
-            }
+            chunkMaps[i] = null;
         }
         //Load region file
         region = RegionDeserializer.LoadMainRegion(fileName, null);
         //Change form title to file name
         Text = $"Region Viewer - {Path.GetFileName(fileName)}";
-        var task = new Task(() => Parallel.For(0, 1024, i =>
-        {
-            int x = i % 32;
-            int z = i / 32;
-            var chunk = region.GetChunk(x, z, ChunkLoadFlags.None);
-            if(chunk != null)
-            {
-                chunk.Load(ChunkLoadFlags.Blocks);
-                var map = (WinformsBitmap)SurfaceMapGenerator.GenerateSurfaceMap(chunk, HeightmapType.AllBlocks, true, WorldForge.Maps.MapColorPalette.Modern, true);
-                chunkMaps[x, z] = map.bitmap;
-                canvas.Invalidate();
-            }
-        }));
-        task.Start();
         scrollNorth.Enabled = RegionExists(currentLocation.North, out _);
         scrollSouth.Enabled = RegionExists(currentLocation.South, out _);
         scrollWest.Enabled = RegionExists(currentLocation.West, out _);
         scrollEast.Enabled = RegionExists(currentLocation.East, out _);
+        Invalidate(true);
+        updateRenderTimer.Start();
     }
 
     private void Draw(object sender, PaintEventArgs e)
     {
+        HandleRenders();
         e.Graphics.Clear(Color.Black);
-        for(int z = 0; z < 32; z++)
+        for (int z = 0; z < 32; z++)
         {
-            for(int x = 0; x < 32; x++)
+            for (int x = 0; x < 32; x++)
             {
-                if(chunkMaps[x, z] != null)
+                int i = z * 32 + x;
+                if (currentChunkRenders.Contains(i)) e.Graphics.FillRectangle(renderingChunkBrush, x * 16, z * 16, 16, 16);
+                else
                 {
-                    e.Graphics.DrawImage(chunkMaps[x, z], x * 16, z * 16);
+                    var map = chunkMaps[i];
+                    if (map != null) e.Graphics.DrawImage(map, x * 16, z * 16);
+                    else if (region.chunks[x,z] != null) e.Graphics.FillRectangle(queuedChunkBrush, x * 16, z * 16, 16, 16);
                 }
             }
         }
-        if(hoveredChunk.x >= 0 && hoveredChunk.z >= 0)
+        if (hoveredChunk.x >= 0 && hoveredChunk.z >= 0)
         {
             e.Graphics.DrawRectangle(Pens.Red, hoveredChunk.x * 16, hoveredChunk.z * 16, 16, 16);
         }
     }
 
+    private void HandleRenders()
+    {
+        while (chunkRenderQueue.Count > 0 && currentChunkRenders.Count < 16)
+        {
+            int i = chunkRenderQueue[0];
+            chunkRenderQueue.RemoveAt(0);
+            int x = i % 32;
+            int z = i / 32;
+            StartChunkRender(x, z);
+        }
+    }
+
+    private void StartChunkRender(int x, int z)
+    {
+        int i = z * 32 + x;
+        lock (currentChunkRendersLock)
+        {
+            currentChunkRenders.Add(i);
+        }
+        Task.Run(() =>
+        {
+            try
+            {
+                var chunk = region.GetChunk(x, z, ChunkLoadFlags.None);
+                if (chunk != null)
+                {
+                    if (!chunk.IsLoaded) chunk.Load(ChunkLoadFlags.Blocks);
+                    var map = (WinformsBitmap)SurfaceMapGenerator.GenerateSurfaceMap(chunk, HeightmapType.AllBlocks, true, WorldForge.Maps.MapColorPalette.Modern, true);
+                    chunkMaps[i] = map.bitmap;
+                    canvas.Invalidate();
+                }
+            }
+            finally
+            {
+                lock (currentChunkRendersLock)
+                {
+                    currentChunkRenders.Remove(i);
+                }
+            }
+        });
+    }
+
     private void OnCanvasClick(object sender, EventArgs e)
     {
         var mouseEvent = (MouseEventArgs)e;
-        if(mouseEvent.Button == MouseButtons.Left)
+        if (mouseEvent.Button == MouseButtons.Left)
         {
-            if(hoveredChunk.x >= 0 && hoveredChunk.z >= 0)
+            if (hoveredChunk.x >= 0 && hoveredChunk.z >= 0)
             {
                 var chunk = region.GetChunk(hoveredChunk.x, hoveredChunk.z);
-                if(chunk != null)
+                if (chunk != null)
                 {
                     var nbt = RegionDeserializer.LoadChunkDataAtIndex(region.sourceFilePaths.mainPath, hoveredChunk.LocalRegionPos.x + hoveredChunk.LocalRegionPos.z * 32);
                     var nbtViewer = new NBTViewer(nbt, $"Chunk {chunk.WorldSpaceCoord}");
@@ -107,21 +163,21 @@ public partial class RegionViewer : Form
             }
         }
         else
-        if(mouseEvent.Button == MouseButtons.Right)
+        if (mouseEvent.Button == MouseButtons.Right)
         {
-	        RegionFileDialog();
+            RegionFileDialog();
         }
     }
 
     private void RegionFileDialog()
     {
-	    OpenFileDialog openFileDialog = new OpenFileDialog();
-	    openFileDialog.Filter = "Region Files (*.mca;*.mcr)|*.mca;*.mcr|All Files (*.*)|*.*";
-	    if(openFileDialog.ShowDialog() == DialogResult.OK)
-	    {
-		    fileName = openFileDialog.FileName;
-		    Load();
-	    }
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.Filter = "Region Files (*.mca;*.mcr)|*.mca;*.mcr|All Files (*.*)|*.*";
+        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            fileName = openFileDialog.FileName;
+            Load();
+        }
     }
 
     private void OnMouseMove(object sender, MouseEventArgs e)
@@ -159,7 +215,7 @@ public partial class RegionViewer : Form
 
     private void TryLoadRegion(RegionLocation location)
     {
-        if(RegionExists(location, out var name))
+        if (RegionExists(location, out var name))
         {
             fileName = name;
             Load();
@@ -180,6 +236,6 @@ public partial class RegionViewer : Form
 
     private void openFile_Click(object sender, EventArgs e)
     {
-		RegionFileDialog();
-	}
+        RegionFileDialog();
+    }
 }
