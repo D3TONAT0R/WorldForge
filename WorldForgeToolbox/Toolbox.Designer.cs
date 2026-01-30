654bd14cafb73af328236c17ed439bb5e7dfda37
D3TONAT0R <gdv@swissonline.ch>
2026-01-30 01:01:00 +0100

    Toolbox layout

--- WorldForgeToolbox/Toolbox.Designer.cs
+++ WorldForgeToolbox/Toolbox.Designer.cs
@@ -22,93 +22,87 @@ namespace WorldForgeToolbox
 			base.Dispose(disposing);
 		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            panel = new FlowLayoutPanel();
-            regionViewer = new Button();
-            nbtViewer = new Button();
-            worldViewer = new Button();
-            blockDistributionViewer = new Button();
-            panel.SuspendLayout();
-            SuspendLayout();
-            // 
-            // panel
-            // 
-            panel.Controls.Add(regionViewer);
-            panel.Controls.Add(nbtViewer);
-            panel.Controls.Add(worldViewer);
-            panel.Controls.Add(blockDistributionViewer);
-            panel.Dock = DockStyle.Fill;
-            panel.Location = new Point(0, 0);
-            panel.Name = "panel";
-            panel.Size = new Size(284, 261);
-            panel.TabIndex = 0;
-            // 
-            // regionViewer
-            // 
-            regionViewer.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            regionViewer.AutoSize = true;
-            regionViewer.Location = new Point(3, 3);
-            regionViewer.Name = "regionViewer";
-            regionViewer.Size = new Size(200, 25);
-            regionViewer.TabIndex = 1;
-            regionViewer.Text = "Region Viewer";
-            regionViewer.UseVisualStyleBackColor = true;
-            regionViewer.Click += OnRegionViewerClick;
-            // 
-            // nbtViewer
-            // 
-            nbtViewer.AutoSize = true;
-            nbtViewer.Location = new Point(3, 34);
-            nbtViewer.Name = "nbtViewer";
-            nbtViewer.Size = new Size(200, 25);
-            nbtViewer.TabIndex = 0;
-            nbtViewer.Text = "NBT Viewer";
-            nbtViewer.UseVisualStyleBackColor = true;
-            nbtViewer.Click += OnNBTViewerClick;
-            // 
-            // worldViewer
-            // 
-            worldViewer.AutoSize = true;
-            worldViewer.Location = new Point(3, 65);
-            worldViewer.Name = "worldViewer";
-            worldViewer.Size = new Size(200, 25);
-            worldViewer.TabIndex = 2;
-            worldViewer.Text = "World Viewer";
-            worldViewer.UseVisualStyleBackColor = true;
-            worldViewer.Click += OnWorldViewerClick;
-            // 
-            // blockDistributionViewer
-            // 
-            blockDistributionViewer.AutoSize = true;
-            blockDistributionViewer.Location = new Point(3, 96);
-            blockDistributionViewer.Name = "blockDistributionViewer";
-            blockDistributionViewer.Size = new Size(200, 25);
-            blockDistributionViewer.TabIndex = 3;
-            blockDistributionViewer.Text = "Block Distribution";
-            blockDistributionViewer.UseVisualStyleBackColor = true;
-            blockDistributionViewer.Click += OnBlockDistributionClick;
-            // 
-            // Toolbox
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(284, 261);
-            Controls.Add(panel);
-            Name = "Toolbox";
-            Text = "Toolbox";
-            panel.ResumeLayout(false);
-            panel.PerformLayout();
-            ResumeLayout(false);
-        }
-
-        private System.Windows.Forms.FlowLayoutPanel panel;
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			regionViewer = new Button();
+			nbtViewer = new Button();
+			worldViewer = new Button();
+			blockDistributionViewer = new Button();
+			SuspendLayout();
+			// 
+			// regionViewer
+			// 
+			regionViewer.Dock = DockStyle.Top;
+			regionViewer.Location = new Point(0, 25);
+			regionViewer.Name = "regionViewer";
+			regionViewer.Size = new Size(244, 25);
+			regionViewer.TabIndex = 1;
+			regionViewer.Text = "Region Viewer";
+			regionViewer.UseVisualStyleBackColor = true;
+			regionViewer.Click += OnRegionViewerClick;
+			// 
+			// nbtViewer
+			// 
+			nbtViewer.AutoSize = true;
+			nbtViewer.Dock = DockStyle.Top;
+			nbtViewer.Location = new Point(0, 0);
+			nbtViewer.Name = "nbtViewer";
+			nbtViewer.Size = new Size(244, 25);
+			nbtViewer.TabIndex = 0;
+			nbtViewer.Text = "NBT Viewer";
+			nbtViewer.UseVisualStyleBackColor = true;
+			nbtViewer.Click += OnNBTViewerClick;
+			// 
+			// worldViewer
+			// 
+			worldViewer.AutoSize = true;
+			worldViewer.Dock = DockStyle.Top;
+			worldViewer.Location = new Point(0, 50);
+			worldViewer.Margin = new Padding(5);
+			worldViewer.Name = "worldViewer";
+			worldViewer.Size = new Size(244, 25);
+			worldViewer.TabIndex = 2;
+			worldViewer.Text = "World Viewer";
+			worldViewer.UseVisualStyleBackColor = true;
+			worldViewer.Click += OnWorldViewerClick;
+			// 
+			// blockDistributionViewer
+			// 
+			blockDistributionViewer.AutoSize = true;
+			blockDistributionViewer.Dock = DockStyle.Top;
+			blockDistributionViewer.Location = new Point(0, 75);
+			blockDistributionViewer.Margin = new Padding(5);
+			blockDistributionViewer.Name = "blockDistributionViewer";
+			blockDistributionViewer.Size = new Size(244, 25);
+			blockDistributionViewer.TabIndex = 3;
+			blockDistributionViewer.Text = "Block Distribution";
+			blockDistributionViewer.UseVisualStyleBackColor = true;
+			blockDistributionViewer.Click += OnBlockDistributionClick;
+			// 
+			// Toolbox
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			AutoSize = true;
+			ClientSize = new Size(244, 101);
+			Controls.Add(blockDistributionViewer);
+			Controls.Add(worldViewer);
+			Controls.Add(regionViewer);
+			Controls.Add(nbtViewer);
+			FormBorderStyle = FormBorderStyle.FixedSingle;
+			MaximizeBox = false;
+			MinimizeBox = false;
+			Name = "Toolbox";
+			Text = "Toolbox";
+			ResumeLayout(false);
+			PerformLayout();
+		}
 		private System.Windows.Forms.Button nbtViewer;
 		private System.Windows.Forms.Button regionViewer;
 
